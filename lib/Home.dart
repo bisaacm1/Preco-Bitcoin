1e0b8f0909634c9e8d37280e4279766bd010edfc
Isaac Batista Maciel <imaciel@server01.foursys.local>
2020-09-13 00:19:13 -0300

    first commit

---
+++ lib/Home.dart
@@ -0,0 +1,67 @@
+﻿import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController _controllerPreco = TextEditingController();
+  String _resultado = "0";
+
+  _recuperarPreco() async {
+    String url = "https://blockchain.info/ticker";
+    http.Response response = await http.get(url);
+    Map<String, dynamic> retorno = json.decode(response.body);
+
+    setState(() {
+      _resultado = retorno['BRL']['buy'].toString();
+    });
+
+    // print(
+    //   "Resposta logradouro: ${logradouro} bairro: ${bairro} ");
+
+    //print("resposta: " + response.statusCode.toString() );
+    //print("resposta: " + response.body );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(bottom: 32),
+              child: Image.asset('images/bitcoin.png'),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 20),
+              child: Text(
+                'R\$' + _resultado,
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: RaisedButton(
+                  color: Colors.orange,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: _recuperarPreco),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
